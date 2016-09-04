54f94d90f67de39beab013c8895bdade1feab4f8
dzucconi <mail@damonzucconi.com>
2016-09-04 13:38:22 +0200

    Clean up using extracted middleware

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -1,30 +1,23 @@
 import page from 'page';
-import qs from './lib/qs';
-import { wait } from './lib/helpers';
-import Headings from './collections/headings';
+
+// Pages
 import data from './pages/data';
 import precis from './pages/precis';
-import render from './lib/render';
 
-const headings = new Headings;
-
-const enrich = (ctx, next) =>
-  wait()
-    .then(() => {
-      ctx.collection = headings;
-      ctx.parsed = qs.parse(location.hash.split('?')[1]);
-      ctx.els = {
-        stage: render(document.getElementById('stage')),
-        indicator: render(document.getElementById('indicator')),
-      };
-
-      next();
-    });
+// Middleware
+import enrich from './middleware/enrich';
+import filter from './middleware/filter';
 
 export default () => {
-  page('*', enrich);
+  page('*',
+    enrich,
+    filter
+  );
+
+  // Routes
   page('/', precis);
   page('/data', data);
+
   page({
     hashbang: true,
   });
