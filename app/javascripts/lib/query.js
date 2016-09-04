ae0e68fb921a0c56cac496816abcd6e5dbb694eb
dzucconi <mail@damonzucconi.com>
2016-09-04 13:23:22 +0200

    Support removal of params via null

--- app/javascripts/lib/query.js
+++ app/javascripts/lib/query.js
@@ -1,4 +1,11 @@
 import qs from './qs';
 
-export default params => (options = {}) =>
-  qs.stringify(Object.assign({}, params, options));
+export default params => (options = {}) => {
+  const query = Object.assign({}, params, options);
+
+  for(var k in query) {
+    if (!query[k]) delete query[k];
+  }
+
+  return qs.stringify(query);
+};
