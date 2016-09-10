8bc40f9401d268dc7722ca62a792a0df88f72d42
dzucconi <mail@damonzucconi.com>
2016-09-10 13:47:07 +0200

    Autoprefix & digest

--- brunch-config.js
+++ brunch-config.js
@@ -6,10 +6,30 @@ module.exports = {
         'app.js': /^app/
       }
     },
-    stylesheets: {joinTo: 'app.css'}
+    stylesheets: {
+      joinTo: 'app.css'
+    }
   },
 
   plugins: {
-    babel: {presets: ['es2015']}
+    babel: {
+      presets: ['es2015']
+    },
+    postcss: {
+      processors: [
+        require('autoprefixer')(['last 8 versions'])
+      ]
+    },
+    digest: {
+      pattern: /DIGEST\(\/?([^\)]*)\)/g,
+      discardNonFilenamePatternParts: true,
+      referenceFiles: /\.html$/,
+      precision: 8,
+      alwaysRun: false,
+      environments: ['production'],
+      prependHost: null,
+      manifest: '',
+      infixes: []
+    }
   }
 };
